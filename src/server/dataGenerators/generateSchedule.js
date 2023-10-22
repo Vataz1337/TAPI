58707e313549045f09effde1c1457576101224e3
Vataz1337 <patryksroka99@gmail.com>
2023-10-23 01:37:28 +0200

    kinda works

--- src/server/dataGenerators/generateSchedule.js
+++ src/server/dataGenerators/generateSchedule.js
@@ -3,11 +3,11 @@ import { faker } from '@faker-js/faker';
 export const generateSchedule = (id) => {
     faker.seed(Number(id));
 
-    const day = faker.date.weekday(); // Generate a random weekday
-    const subject = faker.random.words(); // Generate random subject or topic
-    const room = faker.address.streetName(); // Generate a random room name
-    const startTime = faker.time.recent(); // Generate a random start time
-    const endTime = faker.time.recent(); // Generate a random end time
+    const day = faker.date.weekday();
+    const subject = faker.random.words();
+    const room = faker.address.streetName();
+    const startTime = faker.time.recent();
+    const endTime = faker.time.recent();
 
     return {
         id: id,
--- src/server/routes/student.js
+++ src/server/routes/student.js
@@ -1,12 +1,30 @@
+import fs from "fs";
+import path from "path";
 import express from "express";
 import { generateStudent } from "../dataGenerators/generateStudent.js";
 
 export const studentsRouter = express.Router();
 
-studentsRouter.get('/:id', (req, res) => {
+studentsRouter.get('/notFile/:id', (req, res) => {
     const studentId = req.params.id;
 
     const fakeStudent = generateStudent(studentId);
 
     res.send(fakeStudent);
+});
+
+studentsRouter.get('/file/:id', (req, res) => {
+    const filePath = path.join('..', 'generatedJson', 'students', 'studentData.json');
+
+    const rawData = fs.readFileSync(filePath, 'utf-8');
+    const data = JSON.parse(rawData);
+
+    const id = req.params.id;
+    const student = data.find((student) => student.id === id);
+
+    if (student) {
+        res.send(student);
+    } else {
+        res.status(404).json({ error: "Student not found" });
+    }
 });
