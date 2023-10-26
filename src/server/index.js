f7a4feffb21aaf42abdc548221c3d1dc17cf0d15
Vataz <patryksroka99@gmail.com>
2023-10-26 18:12:30 +0200

    trying to add graphql

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,39 @@
 import express from "express";
-import {createServer} from "node:http";
-
+import {expressMiddleware} from "@apollo/server/express4";
+import {ApolloServer} from "@apollo/server";
+import cors from "cors";
 import {studentsRouter} from "./routes/student.js";
 import {scheduleRouter} from "./routes/schedule.js";
 
 const PORT = 8080;
 const app = express();
-const server = createServer(app)
+
+const typeDefs = `#graphql
+type Student {
+    id: Int
+    name: String
+    surname: String
+    email: String
+}
+`;
+
+const Resolvers = {
+    Query: {
+        students: () => students,
+        student: (parent, args) => students.find(s => s.id === args.id),
+    }
+}
+
+const server = new ApolloServer({typeDefs, Resolvers});
+await server.start();
 
 app.use('/student', studentsRouter)
 app.use('/schedule', scheduleRouter)
+app.use('/graphql', cors(), express.json(), expressMiddleware(server))
+app.use(cors({
+    origin: "*"
+}))
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
 })
